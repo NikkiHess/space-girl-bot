115a2aa628b8da059ef4724281da3f9b940a192e
Nikki <coinslotgaming@gmail.com>
2025-10-06 17:46:30 -0400

    stuff

--- spacegirl.py
+++ spacegirl.py
@@ -4,6 +4,24 @@ the main module for space girl bot
 author:
 Nikki Hess (nkhess@umich.edu)
 """
-from discord.ext import commands
 
-bot = commands.Bot()
+# PyPI modules
+import discord # pycord
+
+# my modules
+from nikki_util import timestamp_print as tsprint
+
+CONFIG_PATH = os.path.join("config", "discord.json")
+with open(CONFIG_PATH, "r") as f:
+    config = json.load(f)
+
+TOKEN = config.get("token")
+
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    tsprint(f"{bot.user} is now ready!")
+
+@bot.slash_command()
+async def
