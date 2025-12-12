45e2349b2f1a6089a21f0c925dcdc029b67b339e
Nikki <coinslotgaming@gmail.com>
2025-12-12 01:32:09 -0500

    :bug: fix shared SQL conn/cursor causing db to be mistaken for "readonly"

--- src/db/driver.py
+++ src/db/driver.py
@@ -10,44 +10,47 @@ import sqlite3
 
 DB_DIR = "database"
 os.makedirs(DB_DIR, exist_ok=True) # create database folder if doesn't exist
-DB_PATH = os.path.join(DB_DIR, "pronunication_dictionary.db")
+DB_PATH = os.path.join(DB_DIR, "spacegirl.db")
 
-CONNECTION = sqlite3.connect(DB_PATH, check_same_thread=False)
-CURSOR = CONNECTION.cursor()
+def get_conn() -> sqlite3.Connection:
+    return sqlite3.connect(DB_PATH, check_same_thread=False)
 
 def init_db() -> None:
     """
     Initializes the SQLite database, populating with tables if necessary
     """
 
-    CURSOR.executescript("""
-                         CREATE TABLE IF NOT EXISTS servers (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            guild_id TEXT UNIQUE NOT NULL
-                         );
+    with get_conn() as connection:
+        cursor = connection.cursor()
 
-                         CREATE TABLE IF NOT EXISTS voices (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            name TEXT UNIQUE NOT NULL
-                         );
+        cursor.executescript("""
+                            CREATE TABLE IF NOT EXISTS servers (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                guild_id TEXT UNIQUE NOT NULL
+                            );
 
-                         CREATE TABLE IF NOT EXISTS translations (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            server_id INTEGER NOT NULL,
-                            voice_id INTEGER NOT NULL,
-                            text TEXT NOT NULL,
-                            pronunciation TEXT NOT NULL,
-                            FOREIGN KEY (server_id) REFERENCES servers (id) ON DELETE CASCADE,
-                            FOREIGN KEY (voice_id) REFERENCES voices (id) ON DELETE CASCADE,
-                            UNIQUE(server_id, voice_id, text)
-                         );
+                            CREATE TABLE IF NOT EXISTS voices (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                name TEXT UNIQUE NOT NULL
+                            );
 
-                         CREATE TABLE IF NOT EXISTS user_settings (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            user_id INTEGER NOT NULL,
-                            chosen_voice_id INTEGER 
-                         )
-                         """)
+                            CREATE TABLE IF NOT EXISTS translations (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                server_id INTEGER NOT NULL,
+                                voice_id INTEGER NOT NULL,
+                                text TEXT NOT NULL,
+                                pronunciation TEXT NOT NULL,
+                                FOREIGN KEY (server_id) REFERENCES servers (id) ON DELETE CASCADE,
+                                FOREIGN KEY (voice_id) REFERENCES voices (id) ON DELETE CASCADE,
+                                UNIQUE(server_id, voice_id, text)
+                            );
+
+                            CREATE TABLE IF NOT EXISTS user_settings (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                user_id INTEGER NOT NULL,
+                                chosen_voice_id INTEGER 
+                            )
+                            """)
     
 def init_server(guild_id: int) -> int:
     """
@@ -60,9 +63,12 @@ def init_server(guild_id: int) -> int:
     - the database's internal ID for the server
     """
 
-    CURSOR.execute("INSERT OR IGNORE INTO servers (guild_id) VALUES (?)", (guild_id,))
-    CURSOR.execute("SELECT id FROM servers WHERE guild_id = ?", (guild_id,))
-    return CURSOR.fetchone()[0]
+    with get_conn() as connection:
+        cursor = connection.cursor()
+
+        cursor.execute("INSERT OR IGNORE INTO servers (guild_id) VALUES (?)", (guild_id,))
+        cursor.execute("SELECT id FROM servers WHERE guild_id = ?", (guild_id,))
+        return cursor.fetchone()[0]
 
 def init_voice(voice_name: str) -> int | None:
     """
@@ -75,11 +81,14 @@ def init_voice(voice_name: str) -> int | None:
     - the database's internal ID for the voice
     """
 
-    CURSOR.execute("INSERT OR IGNORE INTO voices (name) VALUES (?)", (voice_name,))
-    CURSOR.execute("SELECT id FROM voices WHERE name = ?", (voice_name,))
-    
-    row = CURSOR.fetchone()
-    return row[0] if row else None
+    with get_conn() as connection:
+        cursor = connection.cursor()
+
+        cursor.execute("INSERT OR IGNORE INTO voices (name) VALUES (?)", (voice_name,))
+        cursor.execute("SELECT id FROM voices WHERE name = ?", (voice_name,))
+        
+        row = cursor.fetchone()
+        return row[0] if row else None
 
 def init_user_settings(user_id: int) -> int:
     """
@@ -92,9 +101,12 @@ def init_user_settings(user_id: int) -> int:
     - the database's internal ID for the user (settings)
     """
 
-    CURSOR.execute("INSERT OR IGNORE INTO user_settings (user_id) VALUES (?)", (user_id,))
-    CURSOR.execute("SELECT id FROM user_settings WHERE user_id = ?", (user_id,))
-    return CURSOR.fetchone()[0]
+    with get_conn() as connection:
+        cursor = connection.cursor()
+        
+        cursor.execute("INSERT OR IGNORE INTO user_settings (user_id) VALUES (?)", (user_id,))
+        cursor.execute("SELECT id FROM user_settings WHERE user_id = ?", (user_id,))
+        return cursor.fetchone()[0]
 
 def add_pronunciation(guild_id: int, voice_name: str, text: str, pronunciation: str) -> None:
     """
@@ -110,11 +122,14 @@ def add_pronunciation(guild_id: int, voice_name: str, text: str, pronunciation: 
     server_id = init_server(guild_id)
     voice_id = init_voice(voice_name)
 
-    CURSOR.execute("""
-                       INSERT OR REPLACE INTO translations (server_id, voice_id, text, pronunciation)
-                       VALUES (?, ?, ?, ?)
-                   """, (server_id, voice_id, text, pronunciation))
-    CONNECTION.commit()
+    with get_conn() as connection:
+        cursor = connection.cursor()
+            
+        cursor.execute("""
+                        INSERT OR REPLACE INTO translations (server_id, voice_id, text, pronunciation)
+                        VALUES (?, ?, ?, ?)
+                    """, (server_id, voice_id, text, pronunciation))
+        connection.commit()
 
 def get_pronunciation(guild_id: int, voice_name: str, text: str) -> str:
     """
@@ -129,15 +144,18 @@ def get_pronunciation(guild_id: int, voice_name: str, text: str) -> str:
     - result[0] if result else None
     """
 
-    CURSOR.execute("""
-                      SELECT translation.pronunciation
-                      FROM translations translation
-                      JOIN servers server ON translation.server_id = server.id
-                      JOIN voices voice ON translation.voice_id = voice.id
-                      WHERE server.guild_id = ? AND voice.name = ? AND translation.text = ?
-                   """, (guild_id, voice_name, text))
-    result = CURSOR.fetchone()
-    return result[0] if result else None
+    with get_conn() as connection:
+        cursor = connection.cursor()
+            
+        cursor.execute("""
+                        SELECT translation.pronunciation
+                        FROM translations translation
+                        JOIN servers server ON translation.server_id = server.id
+                        JOIN voices voice ON translation.voice_id = voice.id
+                        WHERE server.guild_id = ? AND voice.name = ? AND translation.text = ?
+                    """, (guild_id, voice_name, text))
+        result = cursor.fetchone()
+        return result[0] if result else None
 
 def remove_pronunciation(guild_id: int, voice_name: str, text: str) -> bool:
     """
@@ -152,21 +170,24 @@ def remove_pronunciation(guild_id: int, voice_name: str, text: str) -> bool:
     - True if a translation was removed, False if none existed
     """
 
-    CURSOR.execute("""
-                      DELETE FROM translations
-                      WHERE server_id = (
-                          SELECT s.id FROM servers s WHERE s.guild_id = ?
-                      )
-                      AND voice_id = (
-                          SELECT v.id FROM voices v WHERE v.name = ?
-                      )
-                      AND text = ?
-                   """, (guild_id, voice_name, text))
+    with get_conn() as connection:
+        cursor = connection.cursor()
+            
+        cursor.execute("""
+                        DELETE FROM translations
+                        WHERE server_id = (
+                            SELECT s.id FROM servers s WHERE s.guild_id = ?
+                        )
+                        AND voice_id = (
+                            SELECT v.id FROM voices v WHERE v.name = ?
+                        )
+                        AND text = ?
+                    """, (guild_id, voice_name, text))
 
-    changes = CONNECTION.total_changes
-    CONNECTION.commit()
+        changes = connection.total_changes
+        connection.commit()
 
-    return changes > 0
+        return changes > 0
 
 def list_pronunciations(guild_id: int, voice_name: str) -> dict[str, str]:
     """
@@ -184,35 +205,38 @@ def list_pronunciations(guild_id: int, voice_name: str) -> dict[str, str]:
     # get the voice's pronunciations within that server as a dict
     # return
 
-    # grab server id based on guild_id
-    server_row = CURSOR.execute(
-        f"SELECT id FROM servers WHERE guild_id = ? LIMIT 1", (guild_id,)
-    ).fetchone()
-    # no server? no pronunciations.
-    if not server_row: return {}
-    # otherwise we can grab the server id from col 0 of this row
-    server_id = server_row[0]
+    with get_conn() as connection:
+        cursor = connection.cursor()
 
-    # now get the voice
-    voice_row = CURSOR.execute(
-        "SELECT id FROM voices WHERE name = ? LIMIT 1",
-        (voice_name,)
-    ).fetchone()
-    # no voice? no pronunciations.
-    if not voice_row: return {}
-    # else grab voice id from row
-    voice_id = voice_row[0]
+        # grab server id based on guild_id
+        server_row = cursor.execute(
+            f"SELECT id FROM servers WHERE guild_id = ? LIMIT 1", (guild_id,)
+        ).fetchone()
+        # no server? no pronunciations.
+        if not server_row: return {}
+        # otherwise we can grab the server id from col 0 of this row
+        server_id = server_row[0]
 
-    # get pronunciations as a dict
-    pronunciation_rows = CURSOR.execute(
-        """
-        SELECT text, pronunciation FROM translations WHERE server_id = ? AND voice_id = ?
-        """,
-        (server_id, voice_id)
-    ).fetchall()
+        # now get the voice
+        voice_row = cursor.execute(
+            "SELECT id FROM voices WHERE name = ? LIMIT 1",
+            (voice_name,)
+        ).fetchone()
+        # no voice? no pronunciations.
+        if not voice_row: return {}
+        # else grab voice id from row
+        voice_id = voice_row[0]
 
-    # map and return
-    return {text: pronunciation for text, pronunciation in pronunciation_rows}
+        # get pronunciations as a dict
+        pronunciation_rows = cursor.execute(
+            """
+            SELECT text, pronunciation FROM translations WHERE server_id = ? AND voice_id = ?
+            """,
+            (server_id, voice_id)
+        ).fetchall()
+
+        # map and return
+        return {text: pronunciation for text, pronunciation in pronunciation_rows}
 
 def set_user_voice(user_id: int, voice_name: str) -> None:
     """
@@ -225,13 +249,16 @@ def set_user_voice(user_id: int, voice_name: str) -> None:
 
     voice_id = init_voice(voice_name)
     init_user_settings(user_id) # no need to use internal user ID, just makes things confusing
+    
+    with get_conn() as connection:
+        cursor = connection.cursor()
 
-    CURSOR.execute("""
-                      UPDATE user_settings
-                      SET chosen_voice_id = ?
-                      WHERE user_id = ?
-                   """, (voice_id, user_id))
-    CONNECTION.commit()
+        cursor.execute("""
+                        UPDATE user_settings
+                        SET chosen_voice_id = ?
+                        WHERE user_id = ?
+                    """, (voice_id, user_id))
+        connection.commit()
 
 def get_user_voice(user_id: int) -> str | None:
     """
@@ -243,24 +270,27 @@ def get_user_voice(user_id: int) -> str | None:
     ## Returns:
     - `voice_name` (str | None): the name of the user's default voice (could be nothing)
     """
-
-    # get internal voice id
-    CURSOR.execute("""
-                      SELECT chosen_voice_id
-                      FROM user_settings
-                      WHERE user_id = ?
-                   """, (user_id,))
     
-    row = CURSOR.fetchone()
+    with get_conn() as connection:
+        cursor = connection.cursor()
 
-    # row could be uninitialized or voice_name could be None
-    if not row or row[0] is None:
-        return None
-    
-    # translate voice id to voice name
-    CURSOR.execute("SELECT name FROM voices WHERE id = ?", (row[0],))
-    voice = CURSOR.fetchone()
+        # get internal voice id
+        cursor.execute("""
+                        SELECT chosen_voice_id
+                        FROM user_settings
+                        WHERE user_id = ?
+                    """, (user_id,))
+        
+        row = cursor.fetchone()
 
-    # voice could be None
-    return voice[0] if voice else None
+        # row could be uninitialized or voice_name could be None
+        if not row or row[0] is None:
+            return None
+        
+        # translate voice id to voice name
+        cursor.execute("SELECT name FROM voices WHERE id = ?", (row[0],))
+        voice = cursor.fetchone()
+
+        # voice could be None
+        return voice[0] if voice else None
                    
