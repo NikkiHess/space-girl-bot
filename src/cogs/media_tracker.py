4cb76440e6777ec168917cdc6b0f1fea97d5b7dc
Nikki <coinslotgaming@gmail.com>
2026-05-01 14:07:01 -0400

    :memo: add TODO for media tracker
    - lots of stuff to do! it's time for this bot to become a more general-purpose bot

---
+++ src/cogs/media_tracker.py
@@ -0,0 +1,11 @@
+# TODO: implement this (based on the "to watch" channel in Afzuigkapje)
+# IDEAS: 
+# - command to input media you want to watch
+# - get icon/data for media from some source (IMDb? do they have a public API?)
+# - also link to whatever the data source is if possible, if not fall back on IMDb
+# - maybe allow for changing this link to your preferred service (IMDb, Letterboxd, etc)
+# - somehow get show # of seasons/movie length, then have a progress bar tracking how far in the guild is
+# - statuses: not started/to rewatch/in progress/actively rewatching
+# - command to search by name, status, type (movie/show), etc.
+# - support for YouTube as well, since not everything is on streaming
+# - all of this in nice embeds ideally, utilizing the existing database. embed color probably based on status (or grab the most common color from the show icon)
